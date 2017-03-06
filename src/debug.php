2aae498f0597cd487d1c3b9b07e520b6ed39b1e2
Kiril Kapustin <kiril.kapustin@gmail.com>
2017-03-06 21:46:40 +0200

    detect environment cli/www

--- src/debug.php
+++ src/debug.php
@@ -5,9 +5,23 @@
  * @param mixed $val
  */
 function d($val) {
-    echo '<pre>';
-    print_r($val);
-    echo '</pre>';
+    if(php_sapi_name() == 'cli')
+    {
+        htmlentities(print_r($val));
+        echo PHP_EOL;
+    }
+    else
+    {
+        echo '
+		<div style="border: 1px solid red; background-color: #FFF0F0; padding: 5px;">
+			<pre>';
+
+        htmlentities(print_r($val));
+
+        echo '
+			</pre>
+		</div>';
+    }
 }
 
 /**
