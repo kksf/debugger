6c3156957a894754ffb2696c792e639e1cec455d
Kiril Kapustin <kiril.kapustin@gmail.com>
2017-03-07 20:59:14 +0200

    d() trace back

--- src/debug.php
+++ src/debug.php
@@ -1,23 +1,38 @@
 <?php
 /**
  * Dump data
+ * It is using some decoration according the environment...
  *
  * @param mixed $val
  */
 function d($val) {
+    $backtraceTmp = debug_backtrace();
+    $backtrace = [];
+    foreach($backtraceTmp AS $element) {
+        $backtrace[] = $element['file'] . ': ' . $element['line'];
+    }
+
     if(php_sapi_name() == 'cli')
     {
-        htmlentities(print_r($val));
+        echo str_repeat('-', 50);
         echo PHP_EOL;
-    }
-    else
-    {
+        print_r($val);
+        echo PHP_EOL;
+        echo 'Called from:';
+        echo PHP_EOL;
+        echo implode($backtrace, PHP_EOL);
+        echo PHP_EOL;
+        echo str_repeat('-', 50);
+    } else {
         echo '
 		<div style="border: 1px solid red; background-color: #FFF0F0; padding: 5px;">
 			<pre>';
-
         htmlentities(print_r($val));
-
+        echo PHP_EOL;
+        echo 'Called from:';
+        echo PHP_EOL;
+        echo implode($backtrace, PHP_EOL);
+        echo PHP_EOL;
         echo '
 			</pre>
 		</div>';
@@ -33,3 +48,4 @@ function dd($val) {
     d($val);
     die;
 }
+
